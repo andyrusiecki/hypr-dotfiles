eb09d326b2282df02026378b3f4711a330e0e45f
Andy Rusiecki <andy.rusiecki@gmail>
2025-11-02 16:52:39 -0500

    Adding hyprdynamicmonitors

--- install.sh
+++ install.sh
@@ -15,7 +15,10 @@ if [ -d "$HOME/.config/hypr" ]; then
 fi
 
 mkdir -p "$HOME/.config/hypr"
-echo -e "\$dotfiles = $root\n#\$wallpapers = <Set Wallpaper directory here to use Theme Selector>\n#\$code_dir = <Set Code Directory here to use VS Code Quick Launch>\nsource = \$dotfiles/hypr/hyprland.conf" > $HOME/.config/hypr/hyprland.conf
+echo -e "\$dotfiles = $root\n#\$wallpapers = <Set Wallpaper directory here to use Theme Selector>\n#\$code_dir = <Set Code Directory here to use VS Code Quick Launch>\nsource = \$dotfiles/hypr/hyprland.conf\n\nsource = \~/.config/hypr/monitors.conf\n" > $HOME/.config/hypr/hyprland.conf
+
+# add empty monitor config to avoid errors before running hyprdynamicmonitors
+touch $HOME/.config/hypr/monitors.conf
 
 # hypridle config (requires a symlink)
 ln -s $root/hypr/hypridle.conf $HOME/.config/hypr/hypridle.conf
