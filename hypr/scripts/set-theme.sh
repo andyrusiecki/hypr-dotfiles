10ce131bd3ae9882c0ed67203070fbab3e607506
Andy Rusiecki <andy.rusiecki@gmail>
2025-11-05 20:47:07 -0500

    theme selector now includes built-in themes and remembers current selections

--- hypr/scripts/set-theme.sh
+++ hypr/scripts/set-theme.sh
@@ -6,12 +6,23 @@ if [ -z "$wallpaper_dir" ]; then
   exit 1
 fi
 
+source ~/.cache/wal/colors.sh
+current_wallpaper=$(basename "$wallpaper")
+
 wallpaper_options=""
+selected=-1
+count=0
+
 for wallpaper in $(/usr/bin/ls $wallpaper_dir); do
   wallpaper_options+="$wallpaper\0icon\x1f$(realpath "$wallpaper_dir/$wallpaper")\n"
+
+  if [ "$wallpaper" == "$current_wallpaper" ]; then
+    selected=$count
+  fi
+  ((count++))
 done
 
-result="$(echo -ne "$wallpaper_options" | rofi -config $DOTFILES_DIR/rofi/config.rasi -dmenu -p "Select Wallpaper" -show-icons)"
+result="$(echo -ne "$wallpaper_options" | rofi -config $DOTFILES_DIR/rofi/config.rasi -dmenu -p "Select Wallpaper" -show-icons -selected-row $selected)"
 wallpaper="$wallpaper_dir/$result"
 
 if [ -z "$result" ]; then
@@ -23,9 +34,36 @@ echo "Selected wallpaper: $wallpaper"
 themes_dir=~/.config/wal/colorschemes
 
 custom_option="Generate Color Scheme from Wallpaper"
-theme_options="$custom_option\n$(/usr/bin/ls $themes_dir/dark)"
+# theme_options="$custom_option\n$(/usr/bin/ls $themes_dir/dark)"
 
-colorscheme="$(echo -ne "${theme_options//.json/}" | rofi -config $DOTFILES_DIR/rofi/config.rasi -dmenu -p "Select Theme" -show-icons)"
+# colorscheme="$(echo -ne "${theme_options//.json/}" | rofi -config $DOTFILES_DIR/rofi/config.rasi -dmenu -p "Select Theme" -show-icons)"
+
+cat=""
+theme_options=""
+selected=-1
+count=0
+
+while read -r line; do
+  if ! [[ "$line" =~ ^- ]]; then
+    cat="$(echo "$line" | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")"
+    continue
+  fi
+
+  if [[ "$cat" == *"User Themes:"* || "$cat" == *"Dark Themes:"* ]]; then
+    option="${line//- /}"
+    if [[ "$option" == *" (last used)" ]]; then
+      option="${option// (last used)/}"
+      selected=$count
+    fi
+
+    theme_options+="$option\n"
+    ((count++))
+  fi
+done < <(wal --theme)
+
+((selected++))
+
+colorscheme="$(echo -ne "$custom_option\n$theme_options" | rofi -config $DOTFILES_DIR/rofi/config.rasi -dmenu -p "Select Theme" -show-icons -selected-row $selected)"
 
 if [ -z "$colorscheme" ]; then
   echo "No Color Scheme selected, exiting."
