2835511959d25c37a1f03b08464c3ea05e33ce62
Andy Rusiecki <andy.rusiecki@gmail.com>
2025-12-10 16:15:32 -0500

    updates to terminal cwd script for vscode

--- hypr/scripts/terminal-cwd.sh
+++ hypr/scripts/terminal-cwd.sh
@@ -5,8 +5,8 @@ approved_classes=(
   "kitty"
 )
 
-window_pid=$(hyprctl activewindow | awk '/pid:/ {print $2}')
-window_class=$(hyprctl activewindow | awk '/class:/ {print $2}')
+
+window_class="$(hyprctl activewindow -j | jq -r '.class')"
 
 approved=false
 for approved_class in "${approved_classes[@]}"; do
@@ -20,19 +20,36 @@ if ! $approved; then
   exit 0
 fi
 
-shell_pid=$(pgrep -P "$window_pid" | tail -n1)
-
 cwd="$HOME"
 
-if [[ "$window_class" == "code" ]]; then
-  cwd="$(strings "/proc/$window_pid/cmdline" | tail -n 1)"
-elif [[ -n $shell_pid ]]; then
-  cwd=$(readlink -f "/proc/$shell_pid/cwd" 2>/dev/null)
-fi
+case "$window_class" in
+  "code")
+    # VSCode stores the last opened folder in a JSON file
+    # not always accurate but better than nothing
+    storage="$HOME/.config/Code/User/globalStorage/storage.json"
+    if ! [ -f "$storage" ]; then
+      break
+    fi
 
-if [[ -d $cwd ]]; then
-  echo "$cwd"
-else
-  echo "$HOME"
-fi
+    last_dir="$(jq -r '.windowsState.lastActiveWindow.folder' "$storage")"
+    if [ -z "$last_dir" ] || [ "$last_dir" == "null" ]; then
+      break
+    fi
 
+    last_dir="${last_dir//file:\/\//}"
+    if [ -d "$last_dir" ]; then
+      cwd="$last_dir"
+    fi
+    ;;
+  "kitty")
+    window_pid="$(hyprctl activewindow -j | jq -r '.pid')"
+    shell_pid=$(pgrep -P "$window_pid" | tail -n1)
+    last_dir=$(readlink -f "/proc/$shell_pid/cwd" 2>/dev/null)
+
+    if [ -d "$last_dir" ]; then
+      cwd="$last_dir"
+    fi
+    ;;
+esac
+
+echo "$cwd"
