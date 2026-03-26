a316616660117da9f1a57738bcb199db265ab955
Andy Rusiecki <andy.rusiecki@gmail.com>
2026-03-26 15:53:46 -0400

    adding powermenu binding

--- hypr/scripts/powermenu.sh
+++ hypr/scripts/powermenu.sh
@@ -4,9 +4,37 @@ options="Shutdown\0icon\x1fshutdown\nSuspend\0icon\x1fsuspend\nReboot\0icon\x1fr
 
 selected=$(echo -ne "$options" | rofi -config $DOTFILES_DIR/rofi/config-noinput.rasi -dmenu -mesg "Uptime: $(uptime -p | sed 's/up\s//')")
 
-if [ -z "$selected" ]; then
-  echo "No option selected, exiting."
-  exit 1
-fi
+notify_timeout=5000
+notify_timeout_seconds=$((notify_timeout / 1000))
 
-notify-send "Selected option: $selected"
+cmd=""
+msg=""
+case $selected in
+  "Shutdown")
+    cmd="systemctl poweroff"
+    msg="Shutting down"
+    ;;
+  "Suspend")
+    cmd="systemctl suspend"
+    msg="Suspending"
+    ;;
+  "Reboot")
+    cmd="systemctl reboot"
+    msg="Rebooting"
+    ;;
+  "Logout")
+    cmd="loginctl terminate-user \"\""
+    msg="Logging out"
+    ;;
+  *)
+    echo "No option selected, exiting."
+    exit 1
+  ;;
+esac
+
+notify-send -t $notify_timeout "$msg in $notify_timeout_seconds seconds..."
+sleep $notify_timeout_seconds
+
+if [ -n "$cmd" ]; then
+  $cmd
+fi
